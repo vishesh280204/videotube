5fb503334baa2ce7845670b1f686dbb664cd346a
vishesh280204 <130971127+vishesh280204@users.noreply.github.com>
2026-06-26 14:22:06 +0530

    Add user account management routes for password change, user info, and avatar updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getChannelInfo, getCurrentUser, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 const router =Router()
 
@@ -25,6 +26,20 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser )
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").post(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)  //patch rakha hai updation ke liye
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/ch/:username").get(verifyJWT,getChannelInfo)
+// get kyuki params me se username le rhe h
+router.route("/history").get(verifyJWT,getWatchHistory) // get kyuki user kuch bhej nhi raha
 
 
 
